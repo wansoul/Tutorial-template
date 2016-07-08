16240f0abc8dbcf56ca79390ecdf6e2167b9e697
Wan Souza <contato@wansouza.com>
2016-07-08 12:11:10 -0300

    Add a second help tip
    Add a second help tip to make clear what happens when user click a
    green point

--- js/main.js
+++ js/main.js
@@ -3,9 +3,14 @@ var firstTime = true;
 $(document).ready(function(){
 	$("#link_video").prettyPhoto();
 	$(".blink_ball").toggle();
+	$(".help_tip2").toggle();
 
 	$(".help_btn").click(function(){
 		toggleHelp();
+	});
+
+	$(".blink_ball").click(function(){
+		$(".help_tip2").slideUp();
 	});
 
 });
@@ -21,6 +26,12 @@ function toggleHelp(){
 
 	if (firstTime) {
 		$(".help_tip").hide();
+		$(".help_tip2").slideDown();
 		firstTime = false;
+	} else {
+		if( $(".help_tip2").is(":visible") ){
+			$(".help_tip2").slideUp();
+		}
 	}
+
 }
